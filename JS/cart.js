e138cda7e6a2ecedcb755d87689f5bd4780e73a3
yagompeixoto <71858303+yagompeixoto@users.noreply.github.com>
2021-05-22 21:03:01 -0300

    Add files via upload

---
+++ JS/cart.js
@@ -0,0 +1,53 @@
+showCart = () => {
+  document.getElementById("cart").style.display = "block";
+};
+
+closeCart = () => {
+  document.getElementById("cart").style.display = "none";
+};
+
+let itensN = [];
+let itensP = [];
+
+addCart = (name, valor) => {
+  sessionStorage.setItem("name", name);
+  sessionStorage.setItem("preco", valor);
+  itensN.push(sessionStorage.getItem("name"));
+  itensP.push(sessionStorage.getItem("preco"));
+
+  document.getElementById("cartItensName").innerHTML = `${itensN
+    .map((pos) => {
+      return `
+
+    <p id="${itensN.indexOf(pos)}">${pos}</p>
+    `;
+    })
+    .join("")}`;
+  document.getElementById("cartItensPrice").innerHTML = `${itensP
+    .map((pos) => {
+      return `
+   <p id="${itensP.indexOf(pos)}">${Number(pos)
+        .toFixed(2)
+        .replace(
+          ".",
+          ","
+        )} <span id="remove" onclick="removeItem(${itensP.indexOf(
+        pos
+      )})">X</span></p>
+  
+   
+   `;
+    })
+    .join("")}`;
+};
+
+removeItem = (posicao) => {
+  let removeName = document.getElementById(posicao);
+  removeName.remove();
+
+  let removePrice = document.getElementById(posicao);
+  removePrice.remove();
+
+  itensN.splice(posicao, 1);
+  itensP.splice(posicao, 1);
+};
---
+++ JS/import.js
@@ -0,0 +1,27 @@
+fetch("./JS/api.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    addProd(data);
+  });
+
+function addProd(data) {
+  document.getElementById("produts").innerHTML = `${data
+    .map((prod) => {
+      return `
+            <div  id="${
+              prod.id
+            }" style="justify-content:space-between; flex-direction:column; display:flex">
+                <img src="${prod.image}"></img>
+                <p>${prod.nome}</p>
+                <p2>R$ ${prod.preco.toFixed(2).replace(".", ",")}</p2>
+                <button id='addCart' onclick="addCart('${prod.nome}',${
+        prod.preco
+      })"><h3>+Carrinho</h3></button>
+                <div id="popup"><p>Adicionado ao carrinho</p></div>
+            </div>
+      `;
+    })
+    .join("")}`;
+}
